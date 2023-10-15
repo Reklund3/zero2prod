bc52f4c3fbd152dd25d4d04f309a5fe201813b65
Robert Eklund <robert.eklund3@gmail.com>
2023-10-15 11:09:17 -0500

    Pushing work to test deploying.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ To follow a guided lesson from Zero to Production in Rust by Luca Palmieri
 ### Commands
 curl command for subscribing
 > curl -i -X POST -d "email=rob@test.com&name=Rob" http://localhost:8080/subscriptions
+
+
+
+
+## NOTES
++ Investe time into `rust-musl-builder` to generate tiny containers.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,10 +13,12 @@ pub async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration file.");
-    let pg_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
+    let pg_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application_settings.host, configuration.application_settings.port);
     let listener = TcpListener::bind(address).expect("Failed to bind the server address and port");
     run(listener, pg_pool)?.await
 }
