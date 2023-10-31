16567fccbc4fb66681022b58765e3885c9f4f261
Robert Eklund <robert.eklund3@gmail.com>
2023-10-31 17:04:44 -0500

    adding error messages for confirm flow.

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,51 +1,107 @@
+use std::fmt::{Debug, Display, Formatter};
 use actix_web::web::Data;
-use actix_web::{get, web, HttpResponse};
+use actix_web::{get, web, HttpResponse, ResponseError};
+use actix_web::http::StatusCode;
+use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
+use crate::routes::StoreTokenError;
 
-#[derive(serde::Deserialize)]
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e);
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
+
+
+#[derive(thiserror::Error)]
+enum ConfirmSubscriptionError {
+    #[error("{0}")]
+    Unauthorized(String),
+    #[error(transparent)]
+    OtherError(#[from] anyhow::Error)
+}
+
+impl Debug for ConfirmSubscriptionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for ConfirmSubscriptionError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            ConfirmSubscriptionError::Unauthorized(_) => StatusCode::UNAUTHORIZED,
+            ConfirmSubscriptionError::OtherError(_) => StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+pub struct ConfirmSubscriberError(sqlx::Error);
+
+impl Debug for ConfirmSubscriberError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl Display for ConfirmSubscriberError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "A database error was encountered while trying to confirm the subscriber.")
+    }
+}
+
+impl std::error::Error for ConfirmSubscriberError {}
+
+#[derive(Debug, serde::Deserialize)]
 pub struct Parameters {
     subscription_token: String,
 }
 
 #[get("/subscriptions/confirm")]
-#[tracing::instrument(name = "Confirm a pending subscriber.", skip(parameters, pool))]
-pub async fn confirm(parameters: web::Query<Parameters>, pool: Data<PgPool>) -> HttpResponse {
-    match get_subscriber_id_from_token(&parameters.subscription_token.as_str(), &pool).await {
-        Ok(maybe_id) => match maybe_id {
-            Some(id) => match confirm_subscriber(id, &pool).await {
-                Ok(_) => HttpResponse::Ok().finish(),
-                Err(_) => HttpResponse::InternalServerError().finish(),
-            },
-            None => HttpResponse::Unauthorized().finish(),
-        },
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    }
+#[tracing::instrument(name = "Confirm a pending subscriber.", skip(pool))]
+pub async fn confirm(parameters: web::Query<Parameters>, pool: Data<PgPool>) -> Result<HttpResponse, ConfirmSubscriptionError> {
+    let maybe_subscriber_id = get_subscriber_id_from_token(&parameters.subscription_token.as_str(), &pool)
+        .await
+        .context("Failed to retrieve the subscriber id from the data base.")
+        .unwrap();
+
+    match maybe_subscriber_id {
+        Some(id) => confirm_subscriber(id, &pool).await,
+        None => Err(ConfirmSubscriptionError::Unauthorized("Subscriber id does not exists in postgres.".into()))
+    }.context("Failed to retrieve a valid user id for the provided subscriber token.")?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(name = "Marking the subscriber as confirmed.", skip(pool))]
-async fn confirm_subscriber(subscriber_id: Uuid, pool: &PgPool) -> Result<(), sqlx::Error> {
+async fn confirm_subscriber(subscriber_id: Uuid, pool: &PgPool) -> Result<(), ConfirmSubscriptionError> {
     sqlx::query!(
         r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
         subscriber_id,
     )
     .execute(pool)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .map_err(|e| e)
+    .context("Failed to update the user record to confirmed.")?;
     Ok(())
 }
 
 #[tracing::instrument(
     name = "Fetching the subscriber id with the subscription token.",
-    skip(subscription_token, pool)
+    skip(pool)
 )]
 async fn get_subscriber_id_from_token(
     subscription_token: &str,
     pool: &PgPool,
-) -> Result<Option<Uuid>, sqlx::Error> {
+) -> Result<Option<Uuid>, ConfirmSubscriptionError> {
     let result = sqlx::query!(
         r#"
             SELECT (subscriber_id) FROM subscription_tokens
@@ -55,9 +111,7 @@ async fn get_subscriber_id_from_token(
     )
     .fetch_optional(pool)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .map_err(|e| e)
+    .context("Failed to get the subscriber id from postgres.")?;
     Ok(result.map(|r| r.subscriber_id))
 }
